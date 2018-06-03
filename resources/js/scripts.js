30e2ce1b4d31797cec6014975c8f8d96fc708e07
Vivian Smith <viviancoopersmith@gmail.com>
2018-06-03 16:16:40 +1000

    last round of updates

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -12,5 +12,33 @@ $('.nav-prompt').click(function () {
         $('.nav-inner').attr('aria-expanded', 'true');
     } else {
         $('.nav-inner').attr('aria-expanded', 'false');
-    }
+    };
+
+    if ($('.nav-prompt').text() === 'Open Navigation') {
+        $('.nav-prompt').text('Close Navigation');
+
+    } else {
+        $('.nav-prompt').text('Open Navigation');}
 })
+
+// image gallery
+function imageGallery() {
+    //if there's no image gallery do nothing.
+    if (!$('.image-gallery').length) {
+    return;
+    }
+    $(".image-gallery a").simpleLightbox();
+}
+
+imageGallery();
+
+// nav active class
+function activeMenu() {
+    var url = window.location.href;
+
+    $('.nav-inner a').filter(function() {
+        return this.href == url;
+    }).addClass('active');
+}
+
+activeMenu();
